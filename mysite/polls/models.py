0230c62cef1547af4bc02c2a6bc7dac7c8771500
Merouane Atig <merouane.atig@gmail.com>
2015-03-06 15:24:32 +0100

    Customize the admin change list

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,6 +15,10 @@ class Question(models.Model):
         return self.pub_date >= \
             timezone.now() - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
